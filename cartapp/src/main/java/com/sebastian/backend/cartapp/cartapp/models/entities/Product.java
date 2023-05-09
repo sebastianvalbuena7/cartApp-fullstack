b5e12d2acf8d560ec72e8b79529e23f148c45d03
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-05-09 12:56:14 -0500

    Backend Terminado

--- cartapp/src/main/java/com/sebastian/backend/cartapp/cartapp/models/entities/Product.java
+++ cartapp/src/main/java/com/sebastian/backend/cartapp/cartapp/models/entities/Product.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
