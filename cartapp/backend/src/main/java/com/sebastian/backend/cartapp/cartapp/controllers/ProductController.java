dc102a1d304e8897056188a5516b08202ae1f867
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-05-09 13:34:36 -0500

    Frontend conectado con el backend

--- cartapp/backend/src/main/java/com/sebastian/backend/cartapp/cartapp/controllers/ProductController.java
+++ cartapp/backend/src/main/java/com/sebastian/backend/cartapp/cartapp/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.sebastian.backend.cartapp.cartapp.controllers;
 
 import com.sebastian.backend.cartapp.cartapp.models.entities.Product;
 import com.sebastian.backend.cartapp.cartapp.services.ProductService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/product")
 public class ProductController {
     private final ProductService productService;
